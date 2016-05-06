a2460bfd87101afc3e42508c9d83902aaaf0f1db
Giannis Sakkoulas <sakkoulas@192.168.1.4>
2016-05-06 15:36:38 +0300

    buttons page

---
+++ extra/buttons.php
@@ -0,0 +1,11 @@
+<?= include "../head.inc";?>
+
+<div class="container">
+  <div class="row">
+    <div class="col-md-6">
+      <a href="btn btn-one btn-large"> Just a button</a>
+    </div>
+  </div>
+</div>
+
+<?= include "../foot.inc";?>
