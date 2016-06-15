d9f1614d51ce37cb676b2d8d9140f8e208aeb029
weborange <sakkoulas@gmail.com>
2016-06-15 09:34:26 +0300

    new pages and classes

---
+++ products/product.php
@@ -0,0 +1,24 @@
+<?php include "../headers/header-3.php";?>
+
+<section class="margin-bottom-40">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6">
+                <img  class="zoom img-responsive" src="../src/bike.jpeg" alt="" data-zoom-image="../src/bike.jpeg">
+            </div>
+            <div class="col-md-6">
+                <h1 class="sepeartor-1">Lorem ipsum dolor sit amet, consectetur.</h1>
+                <ul class="list-unstyled">
+                    <li><b>SKU:</b> 23333</li>
+                    <li>Quantity:</li>
+                </ul>
+
+
+                <button class="btn black">Add To Cart </button>
+            </div>
+        </div>
+    </div>
+</section>
+
+
+<?php include "../footers/footer-3.inc";?>
---
+++ products/products-3.php
@@ -0,0 +1,27 @@
+<?php include "../headers/header-3.php";?>
+
+    <div class="container">
+        <div class="row margin-bottom-20">
+
+            <?php for($i =1; $i <= 20; $i++):?>
+
+            <div class="col-md-3">
+                <img class="img-responsive" src="holder.js/263x263" alt="">
+                <div class="text-center margin-top-bottom-10">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                </div>
+                <div class="text-center margin-top-bottom-10">
+                    M2334 <i class="fa fa-shopping-basket  "></i>
+
+</span>
+
+
+                </div>
+            </div>
+            <?php endfor;?>
+        </div>
+    </div>
+
+
+
+<?php include "../footers/footer-3.inc";?>
